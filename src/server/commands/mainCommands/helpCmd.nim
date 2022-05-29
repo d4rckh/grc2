2d560ca2cd2720640839666566975a6298f9510e
d4rckh <35298550+d4rckh@users.noreply.github.com>
2022-05-29 19:39:02 +0300

    add sponsor button

--- src/server/commands/mainCommands/helpCmd.nim
+++ src/server/commands/mainCommands/helpCmd.nim
@@ -5,7 +5,7 @@ import ../../types
 proc execProc(cmd: Command, originalCommand: string, args: seq[string], flags: Table[string, string], server: C2Server) {.async.} =
   if len(args) >= 1:
     for command in server.cli.commands:
-      if command.name == args[0]:
+      if command.name == args[0] or args[0] in command.aliases:
         echo "-- " & command.name & " --"
         echo "Aliases: " & command.aliases.join(", ")
         echo "Category: " & $command.category
