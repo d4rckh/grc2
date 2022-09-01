cb9ecb307e8ee07eff48e440624488ea3ba87e75
d4rckh <35298550+d4rckh@users.noreply.github.com>
2022-09-01 10:39:25 +0300

    remove dead code

--- src/utils.nim
+++ src/utils.nim
@@ -1,40 +1,4 @@
-import std/[macros, os], uuid4
+import uuid4
 
 proc rndStr*: string =
   return $uuid4()
-
-macro importDirectory*(a: static string, b: static string, c: static string): untyped =
-  var bracket = newNimNode(nnkBracket)
-  for x in walkDir(a, relative=true):
-    if x.kind == pcFile:
-      let split = x.path.splitFile()
-      if split.ext == ".nim":
-        echo "importing file " & split.name 
-        bracket.add ident(split.name)
-  let a = newStmtList(
-  newNimNode(nnkImportStmt).add( 
-    newNimNode(nnkInfix).add(
-      ident("/"),
-      newNimNode(nnkPrefix).add(
-        ident(b), 
-        ident(c)
-      ), 
-      bracket
-      )
-  ))
-  return a 
-
-macro loadCommands*(a: static string): untyped =
-  let stmtList = newStmtList()
-  for x in walkDir(a, relative=true):
-    if x.kind == pcFile:
-      let split = x.path.splitFile()
-      if split.ext == ".nim":
-        echo "loading command " & $split.name
-        stmtList.add(
-          newCall(
-            newDotExpr(ident("commands"), ident("add")),
-            newDotExpr(ident(split.name), ident("cmd"))
-          )
-        ) 
-  return stmtList
